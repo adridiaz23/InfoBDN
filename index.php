21a61d783673cc4405d1a40163b6f00af3fa7aef
adridiaz23 <113337223+adridiaz23@users.noreply.github.com>
2022-10-05 09:14:36 +0200

    maqueta index

--- index.php
+++ index.php
@@ -8,44 +8,31 @@
    <title>Index</title>
 </head>
 <body>
-   <div class = "grid-container">   
-      <header>   
-         <img class="logo" src="img/logo_prueba.png">
-         <img class="redes" src="img/yt_transparente.png">
-         <img class="redes" src="img/instagram_transparente.png">
-         <img class="redes" src="img/tiktok_transparente.png">
-         <img class="redes" src="img/twitter_transparente.png">
+
+         <!--<img class="logo" src="img/logo_prueba.png">
+         
          <div class = "acount">
-            <a class = "link-acount" href="login.php">Iniciar sesión</a>
-            <a class = "link-acount" href="registrarse.php">Registrarse</a>
-         </div>
-      </header>
-
-      
-
-      <nav>
-         Buscador jeje
-      </nav>
-      <menu>
-         as
-      </menu>
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   </div>
+            -->
+   <div class = "grid-container">
+        <div class="header">
+            <div id="principal">    
+               <img class="logo" src="img/logo_prueba.png">
+               <img class="redes" src="img/yt_transparente.png">
+               <img class="redes" src="img/instagram_transparente.png">
+               <img class="redes" src="img/tiktok_transparente.png">
+               <img class="redes" src="img/twitter_transparente.png">
+            </div>
+            <div id="sesion"> 
+               <a class = "link-acount" href="login.php">Iniciar sesión</a>
+               <a class = "link-acount" href="registrarse.php">Registrarse</a>
+            </div>
+        </div>
+        <div class="menu1">menu izq</div>
+        <div class="nav">buscad</div>
+        <div class="menu2">menu dere</div>
+        <div class="content">contenido</div>
+        <div class="footer">pie</div>  
+    </div>
 
 </body>
 </html>
